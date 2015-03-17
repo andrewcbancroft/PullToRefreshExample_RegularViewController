b292eb65bcfa12617d4cc476c4cfd839de84630a
Andrew Bancroft <andrewcbancroft@gmail.com>
2015-03-16 22:36:23 -0500

    Initial commit. Implemented a regular view controller with a UITableView as its main view's contents. Implemented a UIRefreshControl to enable pull to refresh with a UITableView (without a UITableViewController).

--- PullToRefreshExample_RegularViewController/ViewController.swift
+++ PullToRefreshExample_RegularViewController/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Movie {
+	let title: String
+	let genre: String
+}
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+	@IBOutlet weak var tableView: UITableView!
+	
+	var movies = [
+		Movie(title: "Lion King", genre: "Animation"),
+		Movie(title: "Star Wars", genre: "Sci-fi")
+	]
+	
+	lazy var refreshControl: UIRefreshControl = {
+		let refreshControl = UIRefreshControl()
+		refreshControl.addTarget(self, action: "handleRefresh:", forControlEvents: UIControlEvents.ValueChanged)
+		
+		return refreshControl
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		self.tableView.addSubview(self.refreshControl)
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	func handleRefresh(refreshControl: UIRefreshControl) {
+		// Do some reloading of data and update the table view's data source
+		// Fetch more objects from a web service, for example...
+		
+		// Simply adding an object to the data source for this example
+		let newMovie = Movie(title: "Serenity", genre: "Sci-fi")
+		movies.append(newMovie)
+		
+		movies.sort() { $0.title < $1.title }
+		
+		self.tableView.reloadData()
+		refreshControl.endRefreshing()
 	}
-
-
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return movies.count
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+		
+		cell.textLabel?.text = movies[indexPath.row].title
+		cell.detailTextLabel?.text = movies[indexPath.row].genre
+		
+		return cell
+	}
 }
-
